bb5ea31fa3b75f26b67238a6287734c694596782
Brad Schrag <brad.schrag@gmail.com>
2018-07-03 19:44:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,7 +57,9 @@ function removeFromCart(item) {
 
 function placeOrder(ccNumber) {
   if (ccNumber) {
-    return `Your total cost is $${total()}, which will be charged to the card ${ccNumber}.`
+    returnString = `Your total cost is $${total()}, which will be charged to the card ${ccNumber}.`
+    setCart([])
+    return returnString
   } else {
     return `Sorry, we don't have a credit card on file for you.`
   }
